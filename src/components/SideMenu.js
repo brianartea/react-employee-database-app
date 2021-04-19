8127c71de67465f987ed079a7fc7132cb43e981b
brianartea <bri@expertdesign.cc>
2021-04-18 20:51:48 -0400

    Hell of a Project!

---
+++ src/components/SideMenu.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { withStyles } from "@material-ui/core";
+
+// withStyles & makeStyles
+
+const style = {
+  sideMenu: {
+    display: "flex",
+    flexDirection: "column",
+    position: "absolute",
+    left: "0px",
+    width: "320px",
+    height: "500%",
+    backgroundColor: "#253053",
+  },
+};
+
+const SideMenu = (props) => {
+  const { classes } = props;
+  return <div className={classes.sideMenu}></div>;
+};
+
+export default withStyles(style)(SideMenu);
